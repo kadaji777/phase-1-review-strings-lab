6d124fdfeca89c25cc12f861bbebf17c0d4e97f0
kadaji777 <roinwani@gmail.com>
2024-04-14 18:09:19 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// Define currentUser
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
